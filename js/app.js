dff3dafe9d2e069444be1b654b73f914a6e89c9e
Jatinokov <Enkhtaivan1206@gmail.com>
2019-10-16 02:56:00 -0700

    gave to each four buttons random number of value for loses or wins every match, and it wuill be setted for random.

--- js/app.js
+++ js/app.js
@@ -26,80 +26,93 @@ $(document).ready(function(){
 // I setted for targeted randon number between from 30 to 100.
 // which is computer picked.
 
-    var ranNum = Math.floor(Math.random()* 70)+30+1;
+var ranNum = Math.floor(Math.random()* 70)+30+1;
     $('#match-Number').html(ranNum);
     // console.log(ranNum);
-// score =0  is the display order; this point I start with 0, 
+                                                    // score =0  is the display order; this point I start with 0, 
     score = 0;
-// then assigned to html h3 element
+                                                    // then assigned to html h3 element
     $('#score').text(" " + score);
-
-// assigned button1 function here once click 
+                                                    // assigned button1 function here once click 
     $(".btn1").on("click", function(){
-// the clicked random number added by same number
+                                                    // the clicked random number added by same number
         score += button1;
-// check win global scope for function 
-        checkWin()  
+                                                    // check win global scope for function 
+        checkWin() 
+        
+        });
+    // console.log(button1); 
+    $(".btn2").on("click", function(){
+        score += button2;
+        checkWin() 
+            
+        });
+        // console.log(button2); 
+    $(".btn3").on("click", function(){  
+        score += button3;
+        checkWin()
     
- });
-   function checkWin(){
-        $('#score').html(score);
-// conditional for loses or Win
+       });
+        // console.log(button3);
+    $(".btn4").on("click", function(){   
+        score += button4;
+        checkWin()
+
+       });
+       // console.log(button4);
+    
+
+function checkWin(){
+    $('#score').html(score);
+        // conditional for loses or Win
         if(score === ranNum){
             wins++;
             $('#win').html(wins);
-            score = 0; 
 
+            score = 0; 
             $('#score').html(score);
         
             ranNum = Math.floor(Math.random()* 70)+30+1;
             $('#match-Number').html(ranNum);
+
+            button1 = Math.floor(Math.random()*10)+1;
+            button2 = Math.floor(Math.random()*10)+1;
+            button3 = Math.floor(Math.random()*10)+1;
+            button4 = Math.floor(Math.random()*10)+1;
+            
 
         }
         else if( score > ranNum){
             // increment loses, reset score, and generate a new random number
             loses++;
             $('#lose').html(loses);
+
             score = 0;
             $('#score').html(score);
 
             ranNum = Math.floor(Math.random()* 70)+30+1;
             $('#match-Number').html(ranNum);
 
-
+            button1 = Math.floor(Math.random()*10)+1;
+            button2 = Math.floor(Math.random()*10)+1;
+            button3 = Math.floor(Math.random()*10)+1;
+            button4 = Math.floor(Math.random()*10)+1;
+       
         } 
     
    }
+//    check()
+//    function check(){
+//        if(wins === 10 ){
+//            alert("GONGRATULATIONS YOU WON.");
+//        }else if(loses === 10){
+//            alert("GAME OVER!");
+//        }
+           
+       
+
+//    }
+
  
-
-    $(".btn2").on("click", function(){
-      score += button2;
-      checkWin() 
-    $('#score').html(score);
-
-    });
-    // console.log(button2);
-
-    $(".btn3").on("click", function(){  
-        score += button3;
-        checkWin()
-    $('#score').html(button3);
-    });
-    // console.log(button3);
-
-    $(".btn4").on("click", function(){   
-        score += button4;
-        checkWin()
-    $('#score').html(button4);
-    });
-    // console.log(button4);
- 
-
-    
-
-
-
-
-
 
 });
